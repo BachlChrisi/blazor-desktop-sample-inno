2c177358ecde66705477de43010223c70184a90f
Christian <chrisibachl13@gmail.com>
2022-01-04 23:49:27 +0100

    Create Inno Install Project

---
+++ BlazorInnoDemo/BlazorInnoDemo.Installer/Scripts/install.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+
+dotnet publish ..\BlazorInnoDemo.Wpf\ -c Release -o .\Publish -p:PublishReadyToRun=true -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
+
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" ".\inno_install_script.iss"
+
+pause
