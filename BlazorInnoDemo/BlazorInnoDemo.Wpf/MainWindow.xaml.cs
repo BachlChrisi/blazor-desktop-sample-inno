680354fe6846fd6cc3de287111a68096639126ab
Christian <chrisibachl13@gmail.com>
2022-01-04 23:03:34 +0100

    Create blazor desktop wpf project

---
+++ BlazorInnoDemo/BlazorInnoDemo.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebviewAppTest.Data;
+
+namespace BlazorInnoDemo.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }      
+    }
+    public partial class Main { }
+}
